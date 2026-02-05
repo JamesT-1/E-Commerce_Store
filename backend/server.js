b3f14b23ad15ebc380e4589b8fc9451cdc9c77c0
JamesT-1 <taxmatzidis1@gmail.com>
2026-02-05 19:01:14 +0200

    Fixed Routing

--- backend/server.js
+++ backend/server.js
@@ -47,9 +47,7 @@ if (process.env.NODE_ENV === "production") {
 
   app.use((req, res, next) => {
     if (!req.path.startsWith("/api")) {
-      res.sendFile(
-        path.resolve(__dirname, "../frontend", "dist", "index.html")
-      );
+      res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
     } else {
       next();
     }
